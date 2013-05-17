009b6134523b33b32c2416fb9da4e602d75bd3ac
Zack Siri <zack@artellectual.com>
2013-05-17 15:28:01 +0700

    fix install script

--- lib/generators/transponder/install/install_generator.rb
+++ lib/generators/transponder/install/install_generator.rb
@@ -19,11 +19,11 @@ module Transponder
       end
 
       def add_setup
-        template 'setup.js.coffee', "app/assets/javascripts/#{js_app_name}-setup.js.coffee"
+        template 'setup.coffee', "app/assets/javascripts/#{js_app_name}-setup.js.coffee"
       end 
 
       def add_manifest
-        template 'manifest.js.coffee', "app/assets/javascripts/#{js_app_name}-manifest.js.coffee"
+        template 'manifest.coffee', "app/assets/javascripts/#{js_app_name}-manifest.js.coffee"
       end
     end
   end
