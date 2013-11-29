e3397ca21155d842a7c55b04275192db8deaeba2
Zack Siri <zack@artellectual.com>
2013-11-29 18:39:46 +0700

    fix type

--- lib/generators/transponder/install/install_generator.rb
+++ lib/generators/transponder/install/install_generator.rb
@@ -3,16 +3,15 @@ require 'generators/transponder'
 module Transponder
   module Generators
     class InstallGenerator < Base
-      class_option :type,   type: :string, default: 'basic', desc: "Type of app to generate", aliases: "-t"
+      class_option :type,   type: :string, default: 'basic', desc: "type of app to generate", aliases: "-t"
       class_option :shared, type: :boolean, default: false
  
       def create_application
-        directory "#{type}",     "app/assets/javascripts/#{file_name}"
+        directory "#{options[:type]}",     "app/assets/javascripts/#{file_name}"
       end
 
       def add_setup
-        setup_type = options[:full] ? "#{type}_setup" : "#{type}_setup"
-        template "#{type}.coffee", "app/assets/javascripts/#{file_name}/initializers/setup.coffee"
+        template "#{options[:type]}.coffee", "app/assets/javascripts/#{file_name}/initializers/setup.coffee"
       end
 
       def add_manifest
@@ -20,11 +19,11 @@ module Transponder
       end
 
       def add_shared
-        template "#{type}_shared.coffee", "app/assets/javascripts/#{file_name}/#{file_name}.coffee" if options[:shared]
+        template "#{options[:type]}_shared.coffee", "app/assets/javascripts/#{file_name}/#{file_name}.coffee" if options[:shared]
       end
 
       def add_application
-        template "#{type}.coffee", "app/assets/javascripts/#{file_name}.coffee" unless options[:shared]
+        template "#{options[:type]}.coffee", "app/assets/javascripts/#{file_name}.coffee" unless options[:shared]
       end
     end
   end
