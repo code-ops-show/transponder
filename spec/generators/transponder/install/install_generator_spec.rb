d0a6d0b81216980766a6847d310027ffdd41def4
Zack Siri <zack@artellectual.com>
2013-11-30 02:45:14 +0700

    added more tests or install_generator

--- spec/generators/transponder/install/install_generator_spec.rb
+++ spec/generators/transponder/install/install_generator_spec.rb
@@ -60,4 +60,67 @@ describe Transponder::Generators::InstallGenerator do
       it { should contain /Application.run_ready/ }
     end
   end
+
+  describe "utilities with -t minimal --shared" do 
+    before do 
+      run_generator %w(utilities -t minimal --shared)
+    end
+
+    describe "utilities/initializers/boot.coffee" do 
+      subject { file('app/assets/javascripts/utilities/initializers/boot.coffee') }
+      it { should exist }
+      it { should contain /services/ }
+    end
+
+    describe "utilities/initializers/setup.coffee" do 
+      subject { file('app/assets/javascripts/utilities/initializers/setup.coffee') }
+      it { should exist }
+      it { should contain /window.Utilities/ }
+      it { should contain /Helpers/    }
+      it { should contain /Services/   }
+    end
+
+    describe "utilities/utilities.coffee" do 
+      subject { file('app/assets/javascripts/utilities/utilities.coffee') }
+      it { should exist }
+    end
+  end
+
+  describe "management with -t full" do 
+    before do 
+      run_generator %w(management -t full)
+    end
+
+    describe "management/initializers/setup.coffee" do 
+      subject { file('app/assets/javascripts/management/initializers/setup.coffee') }
+      it { should exist }
+      it { should contain /window.Management/ }
+      it { should contain /Helpers/    }
+      it { should contain /Services/   }
+      it { should contain /Presenters/ }
+      it { should contain /Models/ }
+      it { should contain /Collections/ }
+      it { should contain /Views/ }
+    end
+
+    describe "management/models" do 
+      subject { file('app/assets/javascripts/management/models/.keep') }
+      it { should exist }
+    end
+
+    describe "management/collections" do 
+      subject { file('app/assets/javascripts/management/collections/.keep') }
+      it { should exist }
+    end
+
+    describe "management/views" do 
+      subject { file('app/assets/javascripts/management/views/.keep') }
+      it { should exist }
+    end
+
+    describe "management/templates" do 
+      subject { file('app/assets/javascripts/management/templates/.keep') }
+      it { should exist }
+    end
+  end
 end
