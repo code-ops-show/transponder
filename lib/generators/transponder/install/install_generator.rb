8d425a8f534da1621b85d6a1408ba8dad4d9ba38
Zack Siri <zack@artellectual.com>
2013-11-29 14:59:49 +0700

    fix app type for directory

--- lib/generators/transponder/install/install_generator.rb
+++ lib/generators/transponder/install/install_generator.rb
@@ -6,17 +6,20 @@ module Transponder
       class_option :full, type: :boolean, default: false
  
       def create_application
-        directory 'application',     "app/assets/javascripts/#{file_name}"
+        directory "#{app_type}",     "app/assets/javascripts/#{file_name}"
       end
 
       def add_setup
-        setup_type = options[:full] ? 'full_setup' : 'basic_setup'
+        setup_type = options[:full] ? "#{app_type}_setup" : "#{app_type}_setup"
         template "#{setup_type}.coffee", "app/assets/javascripts/#{file_name}/initializers/setup.coffee"
       end
 
       def add_application
-        app_type = options[:full] ? 'full' : 'basic'
         template "#{app_type}.coffee", "app/assets/javascripts/#{file_name}.coffee"
+      end
+
+      def app_type
+        options[:full] ? 'full' : 'basic'
       end
     end
   end
