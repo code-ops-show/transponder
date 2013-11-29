7af4c33da84c51de4e5c8aa51ad87fb7be4a0db7
Zack Siri <zack@artellectual.com>
2013-11-29 13:35:56 +0700

    moved manifest and setup to initializers folder

---
+++ lib/generators/transponder/presenter/presenter_generator.rb
@@ -0,0 +1,10 @@
+require 'generators/transponder'
+require 'generators/transponder/resource_helpers'
+
+module Transponder
+  module Generators
+    class PresenterGenerator < Base
+      
+    end
+  end
+end
