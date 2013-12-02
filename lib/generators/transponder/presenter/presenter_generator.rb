8317ba2e584e08fe2aadc1284d619bfb4721ae23
Zack Siri <zack@artellectual.com>
2013-12-02 17:17:53 +0700

    presenter generator now initializes it as well [#5]

--- lib/generators/transponder/presenter/presenter_generator.rb
+++ lib/generators/transponder/presenter/presenter_generator.rb
@@ -8,6 +8,12 @@ module Transponder
       def add_presenter
         template "_presenter.coffee", "app/assets/javascripts/#{options[:module_name]}/presenters/#{file_name}_presenter.coffee"
       end
+
+      def add_presenter_to_boot
+        insert_into_file "app/assets/javascripts/#{options[:module_name]}/initializers/boot.coffee", 
+                         "new #{options[:module_name].camelize}.Presenters.#{file_name.camelize}Presenter()\n",
+                         after: "# presenters\n"
+      end
     end
   end
 end
