df0f789c8181993f1fe8540f4baf239091958e6e
Zack Siri <zack@artellectual.com>
2013-12-02 18:54:16 +0700

    cleaned up install generator

--- lib/generators/transponder/presenter/presenter_generator.rb
+++ lib/generators/transponder/presenter/presenter_generator.rb
@@ -6,7 +6,8 @@ module Transponder
       class_option :module_name, type: :string, aliases: '-m', default: 'application'
 
       def add_presenter
-        template "_presenter.coffee", "#{javascripts_path}#{options[:module_name]}/presenters/#{file_name}_presenter.coffee"
+        presenter_path = File.join(javascripts_path, options[:module_name], "presenters/#{file_name}_presenter.coffee")
+        template "_presenter.coffee", presenter_path
       end
 
       def presenter_class_name
@@ -14,7 +15,8 @@ module Transponder
       end
 
       def add_presenter_to_boot
-        boot_path = File.join(javascripts_path, options[:module_name, 'initializers/boot.coffee')
+        boot_path = File.join(javascripts_path, options[:module_name], 'initializers/boot.coffee')
+        puts boot_path
         insert_into_file boot_path, 
                          "new #{presenter_class_name}()\n",
                          after: "# presenters\n"
--- spec/generators/transponder/presenter/presenter_generator_spec.rb
+++ spec/generators/transponder/presenter/presenter_generator_spec.rb
@@ -17,11 +17,18 @@ describe Transponder::Generators::PresenterGenerator do
     subject { file('app/assets/javascripts/application/presenters/comments_presenter.coffee') }
   
     before do 
+      FileUtils.cp_r 'spec/fixtures/presenter/app', destination_root
+
       run_generator %w(comments)
     end
 
     describe "comments_presenter.coffee" do 
       it { should exist }
+      it { should contain /CommentsPresenter/ }
+    end
+
+    describe "initializers/boot.coffee" do 
+      it { should contain /CommentsPresenter()/ }
     end
   end
 end
