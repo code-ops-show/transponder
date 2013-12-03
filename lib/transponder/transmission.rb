f481febbafaceab6134bba1caf7e59ff9f153452
Zack Siri <zack@artellectual.com>
2013-12-03 16:53:10 +0700

    cleaned up error handling

--- lib/transponder/transmission.rb
+++ lib/transponder/transmission.rb
@@ -4,18 +4,24 @@ module Transponder
 
     included do 
       after_filter :intercept_js
+      helper_method :xms_module_name
+    end
 
-      def xms_error message: nil, action: nil
-        render json: { errors: message || 'no error message passed', 
-                       controller: controller_name, 
-                       action: action || action_name, 
-                       module: (params[:controller].include?('/') ? params[:controller].split('/')[0] : nil) }, status: :unprocessable_entity
-      end
+    def xms_module_name
+      params[:controller].include?('/') ? params[:controller].split('/')[0] : nil
+    end
 
-      def intercept_js
-        if request.format == :js
-          response.header['Content-Type'] = "application/transmission"
-        end
+
+    def xms_error message: nil, action: nil, module_name: nil
+      render json: { errors: message || 'no error message passed', 
+                     controller: controller_name, 
+                     action: action || action_name, 
+                     module: module_name || xms_module_name }, status: :unprocessable_entity
+    end
+
+    def intercept_js
+      if request.format == :js
+        response.header['Content-Type'] = "application/transmission"
       end
     end
   end
