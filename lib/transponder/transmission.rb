9224b1c9e82df165a1b81858bcd5da1b5eff409e
ChiragRajk <chiragrajk@gmail.com>
2017-01-12 17:32:42 +0700

    change after_filter to after_action

--- lib/transponder/transmission.rb
+++ lib/transponder/transmission.rb
@@ -2,8 +2,8 @@ module Transponder
   module Transmission
     extend ActiveSupport::Concern
 
-    included do 
-      after_filter :intercept_js
+    included do
+      after_action :intercept_js
       helper_method :xms_module_name
     end
 
@@ -12,25 +12,25 @@ module Transponder
     end
 
     def xms_error_hash(object, message, action, module_name)
-      { 
-        errors: (message || xms_error_partial || object.errors ), 
-        controller: controller_name, 
-        action: (action || action_name), 
+      {
+        errors: (message || xms_error_partial || object.errors ),
+        controller: controller_name,
+        action: (action || action_name),
         model_name: object.class.name.downcase,
         id: object.id,
-        module: (module_name || xms_module_name) 
+        module: (module_name || xms_module_name)
       }
     end
 
     def xms_error_partial(module_name: nil, action: nil, controller: nil)
-      render_to_string [(module_name || xms_module_name), 
+      render_to_string [(module_name || xms_module_name),
                         (controller || controller_name), 'errors'].join('/'), layout: false
     rescue ActionView::MissingTemplate
       nil
     end
 
     def xms_error  object, message: nil, action: nil, module_name: nil
-      render json: xms_error_hash(object, message, action, module_name), 
+      render json: xms_error_hash(object, message, action, module_name),
                    status: :unprocessable_entity
     end
 
